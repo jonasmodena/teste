4a0791380e01c177dcfbcfbf10f462c881f4e465
jonasmodena <jonasmodenasouza@gmail.com>
2026-02-01 18:38:57 -0400

    Fix settings for Render build

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default=SECRET_KEY)  # usa a sua local se não tiver env
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me")  # usa a sua local se não tiver env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
